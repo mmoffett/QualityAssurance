7c9c088cf5834cf65a6e606e2b25831132ad20f8
ADMIN\mmoffett18 <marissamoffett@gmail.com>
2016-09-28 13:46:47 -0700

    Re made Waze Project Coded UI

---
+++ CodedUITests/Waze/Waze/CodedUITest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace Waze
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        private ApplicationUnderTest testapp;
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            //"\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\""
+            // testapp = ApplicationUnderTest.Launch(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe\");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+        [TestCleanup()]
+        public void myTestCleanUp()
+        {
+            testapp.Close();
+        }
+        
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+    }
+}
