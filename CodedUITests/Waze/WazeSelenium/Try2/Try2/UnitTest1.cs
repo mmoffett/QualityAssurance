a66b21f44fcbdee1b1cfdcc0ab1874614190a9c4
mmoffett <marissamoffett@gmail.com>
2016-10-12 12:18:23 -0700

    Began Selenium Testing

---
+++ CodedUITests/Waze/WazeSelenium/Try2/Try2/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+namespace seleniumDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driverGC;
+        static IWebDriver driverFF;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverFF = new FirefoxDriver();
+            driverGC = new ChromeDriver(@"C:\Users\mmoffett18\desktop\chromedriver");
+        }
+        [TestMethod]
+        public void TestChromeDriver()
+        {
+            driverGC.Navigate().GoToUrl("http://www.google.com");
+            driverGC.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+            driverGC.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+        [TestMethod]
+        public void TestFFDriver()
+        {
+            driverFF.Navigate().GoToUrl("http://www.google.com");
+            driverFF.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+            driverFF.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ CodedUITests/Waze/WazeSelenium/Waze/Waze/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Waze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //!Make sure to add the path to where you extracting the chromedriver.exe:
+            IWebDriver driver = new ChromeDriver(@"C:\Users\mmoffett18\Desktop\chromedriver"); //<-Add your path
+            driver.Navigate().GoToUrl("http://www.joecolantonio.com/HpSupport.html");
+        }
+    }
+}
